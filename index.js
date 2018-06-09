eb33cfb1dcde31a4b90d09f2465d22fa0c8ef668
Rashel Burton <rashelburton@gmail.com>
2018-06-09 02:59:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function happyHolidays() {
+<<<<<<< HEAD
   return ("Happy holidays!")
 }
 function happyHolidaysTo(name) {
@@ -9,4 +10,10 @@ function happyHolidayTo(holiday, name) {
 }
 function holidayCountdown(holiday, days) {
   return (`It's ${days} days until ${holiday}!`)
+=======
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays!`
+>>>>>>> ec4c356946839d6b1a34c41fd356a0151409fafe
 }
